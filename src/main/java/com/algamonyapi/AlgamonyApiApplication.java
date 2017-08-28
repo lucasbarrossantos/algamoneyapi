4f91f3f22693339cf4a55feef330c9785bf608d0
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-08-27 21:54:48 -0300

    Criacao do projeto

---
+++ src/main/java/com/algamonyapi/AlgamonyApiApplication.java
@@ -0,0 +1,13 @@
+package com.algamonyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamonyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamonyApiApplication.class, args);
+	}
+
+}
