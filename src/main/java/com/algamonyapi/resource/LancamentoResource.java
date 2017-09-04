0c62a5266b3430ba295cd0fd52ecd88eba9f9696
Lucas Barros <lucas-barros28@hotmail.com>
2017-09-04 16:35:14 -0300

    Cadastro de lançamento

--- src/main/java/com/algamonyapi/resource/LancamentoResource.java
+++ src/main/java/com/algamonyapi/resource/LancamentoResource.java
@@ -1,14 +1,16 @@
 package com.algamonyapi.resource;
 
+import com.algamonyapi.event.RecursoCriadoEvent;
 import com.algamonyapi.model.Lancamento;
 import com.algamonyapi.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,8 +20,18 @@ public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listar(){ return lancamentoRepository.findAll(); }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> salvar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+    }
 
     @GetMapping("/{codigo}")
     public ResponseEntity<?> buscarPeloCodigo(@PathVariable("codigo") Long codigo){
